5c87edab19835dc5b46f04da78ad2976d292b7ca
iou-shiuan <tinaliu555@gmail.com>
2016-06-09 09:59:25 +0800

    first upload

---
+++ js/class(original).js
@@ -0,0 +1,341 @@
+class ImageDealer {
+  constructor() {
+    this._imgRoot = ImageDealer.REF.storage().ref();
+    this._uploadTask;
+
+  }
+  uploadImage(imgFile, path){
+    this._uploadTask = this._imgRoot.child("images/" + path ).put(imgFile);
+  }
+  uploadListener(progressM, completeM){
+    this._uploadTask.on('state_changed',
+    function (snapshot) {
+      var uploadPro = snapshot.bytesTransferred / snapshot.totalBytes;
+      progressM(uploadPro);
+    },function (error) {
+      alert(error.code);
+    },function () {
+      completeM();
+    });
+  }
+  getImage(path,dom){
+    this._imgRoot.child("images/"+path).getDownloadURL().then(function (url) {
+      dom.css("background-image","url("+url+")");
+    }).catch(function (error) {
+      alert(error.code);
+    });
+  }
+  deleteImage(path, dom){
+    this._imgRoot.child("images/"+path).delete().then(function () {
+      dom.css("background-image","url()");
+    }).catch(function (error) {
+      alert(error.code);
+    })
+  }
+}
+
+class Item {
+  constructor(data, isSeller) {
+    this._title = "Unknown";
+    this._price = 0;
+    this._sellerKey ="";
+    this._sellerName = "Unknown";
+    this._itemKey = "";
+    this._isSeller = false;
+    this._itemImg = new ImageDealer();
+    this._domView;
+    this._checkInputCreate(data, isSeller);
+  }
+  _checkInputCreate(data, isSeller){
+    for (var variable in data) {
+      switch (variable) {
+        case "title":
+          if (typeof data[variable] === "string" ) {
+            this._title = data[variable];
+          }
+          break;
+        case "price":
+          if (typeof data[variable] === "number" ) {
+            this._price = data[variable];
+          }
+          break;
+        case "itemKey":
+          if (typeof data[variable] === "string" ) {
+            this._itemKey = data[variable];
+          }
+          break;
+        case "seller":
+          if (typeof data[variable] === "string" ) {
+            this._sellerKey = data[variable];
+          }
+          break;
+        case "sellerName":
+          if (typeof data[variable] === "string" ) {
+            this._sellerName = data[variable];
+          }
+          break;
+      }
+    }
+    if (isSeller) {
+      if (isSeller.uid == this._sellerKey) {
+        this._isSeller= true;
+      }
+    }
+    this._domView = this._createItem();
+  }
+  _createItem() {
+    var picPart = this._createPic();
+    var wordPart = this._createIntro();
+    var itemView = $("<div>",{
+      class: "sale-item",
+    }).append(picPart).append(wordPart);
+    return itemView;
+  }
+  _picBack() {
+    var pixBa = $("<div>",{class: "pic"});
+    var imgUrl = this._itemImg.getImage((this._sellerKey+"/"+this._itemKey+ "/01.jpg"),pixBa);
+    return pixBa;
+  }
+  _createPic() {
+    if (this._isSeller) {
+      var picDom = this._picBack().append($("<div>",{class: "white-mask"}).append(
+        $("<div>",{class: "option"}).append(
+          $("<h6>", {text: "view", class: "view"})
+        ).append($("<h6>", {text: "edit", class: "edit"}))
+        )
+        );
+      }else {
+        var picDom = this._picBack().append($("<div>",{class: "white-mask"}).append(
+          $("<div>",{class: "option"}).append(
+            $("<h6>",{text: "view", class:"view"})
+          )
+        ));
+      }
+    return picDom;
+  }
+  _createIntro() {
+    // async data flow write this way
+    var authDom = $("<a>",{href: "#", text: this._sellerName});
+    var wordDom = $("<div>", {class: "word"}).append(
+      $("<div>", {class: "name-price"}).append(
+        $("<p>",{text: this._title})
+      ).append(
+        $("<p>",{text: '$' + this._price})
+      )
+    ).append(
+      $("<div>", {class: "seller"}).append(
+        authDom
+      )
+    );
+    return wordDom;
+  }
+  get dom(){
+    return this._domView;
+  }
+  get editBtn(){
+    if (this._isSeller) {
+      return this._domView.find(".edit");
+    }else {
+      return null;
+    }
+  }
+  get viewBtn(){
+    return this._domView.find(".view");
+  }
+  get itemKey(){
+    if (this._itemKey != "") {
+      return this._itemKey;
+    }else {
+      return "undefined";
+    }
+  }
+  get sellerKey(){
+    if (this._sellerKey != "") {
+      return this._sellerKey;
+    }else {
+      return "undefined";
+    }
+  }
+}
+
+class UploadModal {
+  constructor(targetModal) {
+    this._modalDom = targetModal;
+    this._loadPic = new ImageDealer();
+    this._modalDom.on('hidden.bs.modal',this._clear);
+    this._modalDom.find(".picBox").click(this._upload);
+    this._currentItemKey="";
+  }
+  callImage(itemKey, sellerKey){
+    this._loadPic.getImage((sellerKey+"/"+ itemKey+ "/01.jpg"), this._modalDom.find(".picBox"));
+  }
+
+  _clear(){
+    $("#item-info :input").val("");
+    $("#picData").val("");
+    $("#ModalLabel").text("New Item");
+    $("#editData").css("display","none");
+    $("#removeData").css("display","none");
+    $("#submitData").css("display","inline-block");
+    $("#upload-modal .picBox").css("background-image", "url()")
+    $("#progress").css("display","none");
+    this._currentItemKey = "";
+  }
+  _upload(){
+    $(this).siblings("#picData").trigger("click");
+    $(this).siblings("#picData").change(function () {
+      var thumbF = new FileReader();
+      var imgF= $("#picData")[0].files[0];
+      thumbF.readAsDataURL(imgF);
+      thumbF.onloadend = (function (imge) {return function (e) {
+          $(".picBox").css("background-image", "url("+ e.target.result +")");
+      }})(imgF);
+    });
+  }
+  submitPic(sellerKey){
+    var root = this;
+    this._loadPic.uploadImage($("#picData")[0].files[0], sellerKey+"/"+ this._currentItemKey+ "/01.jpg");
+    this._loadPic.uploadListener(function (progress) {
+      $("#progress").css("display","block");
+    },function () {
+      root._modalDom.modal("hide");
+      $("#progress").css("display","none");
+    })
+  }
+  editData(data){
+    $("input:nth-of-type(1)").val(data.title);
+    $("input:nth-of-type(2)").val(data.price);
+    $("textarea").val(data.descrip);
+    $("#ModalLabel").text("Edit Item");
+    $("#submitData").css("display","none");
+    $("#editData").css("display","inline-block");
+    $("#removeData").css("display","inline-block");
+    $("#upload-modal").modal("show");
+    if (data.hasOwnProperty('itemKey')) {
+      this._currentItemKey = data.itemKey;
+    }
+  }
+  deletePic(sellerKey){
+    this._loadPic.deleteImage((sellerKey+"/"+this._currentItemKey+"/01.jpg"),$("#picBox"));
+    this._modalDom.modal("hide");
+  }
+  set itemKey(ite){
+    if (typeof ite === "string") {
+      this._currentItemKey = ite;
+    }
+  }
+  get itemKey(){
+    return this._currentItemKey;
+  }
+
+}
+
+class ViewModal {
+  constructor(targetModal) {
+    this._modalDom = targetModal;
+    this._loadPic = new ImageDealer();
+    var root = this;
+    this._modalDom.on('hidden.bs.modal',function () {
+      root._clear()
+    });
+    this._itemKey="";
+  }
+  _clear(){
+    this._modalDom.find(".title").text("Item Name");
+    this._modalDom.find(".price").text("$ 0");
+    this._modalDom.find(".descrip").text("");
+    this._modalDom.find(".photo").css("background-image","url()");
+    this._modalDom.find("#message").empty();
+    firebase.database().ref("messages/"+this._itemKey).off();
+  }
+  callImage(itemKey,sellerKey){
+    console.info(this);
+    this._loadPic.getImage((sellerKey+ "/"+ itemKey+ "/01.jpg"), this._modalDom.find(".photo"));
+    this._itemKey = itemKey;
+  }
+  writeData(data){
+    this._modalDom.find(".title").text(data.title);
+    this._modalDom.find(".price").text("$"+ data.price);
+    this._modalDom.find(".descrip").text(data.descrip);
+    this._modalDom.modal("show");
+  }
+}
+
+class MessageBox {
+  constructor(currentuser, itemKey) {
+    this._hasLoggin = false;
+    this._currUser ;
+    this._dialogDom;
+    this._inputBox;
+    this._submitFunc;
+    this._itemKey;
+    this._dialogs =[];
+    this._checkAndAdd(currentuser, itemKey);
+  }
+  _checkAndAdd(currentuser, itemKey){
+    if (currentuser != null ) {
+      this._currUser = currentuser
+      this._hasLoggin =true;
+    }
+    if (itemKey != "undefined") {
+      this._itemKey = itemKey;
+    }
+    this._dialogDom = $("<div>", {class: "messages"});
+    if (this._hasLoggin) {
+      var root = this;
+      this._inputBox = $("<div>",{class:"media"}).append(
+        $("<div>",{class: "media-left"}).append($("<img>",{class:"media-object", src: this._currUser.photoURL, alt: this._currUser.displayName}))
+      ).append($("<div>",{class:"media-body"}).append($("<h4>",{class:"media-heading", text: this._currUser.displayName})).append($("<input>",{id:"dialog"})));
+
+    }
+
+  }
+  addDialog(data){
+    this._dialogs.push(data);
+    this._dialogs.sort(function (a,b) {
+      if(a.time> b.time){ return 1;}
+      if (a.time< b.time) {return -1}
+      return 0;
+    });
+
+    $(".messages").empty();
+    for (var i = 0; i < this._dialogs.length; i++) {
+      var sinDia = this._dialogs[i];
+      this._addDialog(sinDia.message, sinDia.name, sinDia.picURL);
+    }
+  }
+  _addDialog(message, userName, picURL){
+    var sinDialog = $("<div>", {class: "media"}).append($("<div>",{class:"media-left"}).append($("<img>",{class: "media-object", src: picURL, alt: userName}))).append($("<div>",{class:"media-body"}));
+    sinDialog.children(".media-body").append($("<h4>", {text:userName, class:"media-heading"})).append($("<p>",{text: message}));
+    $(".messages").append(sinDialog);
+  }
+  refresh(){
+    this._dialogDom.find(".messages").empty();
+    // for (var i = 0; i < this._dialogs.length; i++) {
+    //   this._dialogs.pop();
+    // }
+    this._dialogs = [];
+
+  }
+  set submitFunction(subFunc){
+    this._submitFunc = subFunc;
+    
+  }
+  get dom(){
+    return this._dialogDom;
+  }
+  get inputBox(){
+    if (this._hasLoggin) {
+      return this._inputBox;
+    }else {
+      return false;
+    }
+  }
+  get userName(){
+    return this._currUser.displayName;
+  }
+  get itemKey(){
+    return this._itemKey;
+  }
+}
+
---
+++ js/main.js
@@ -0,0 +1,352 @@
+var config = {
+    apiKey: "AIzaSyAvIBhKJushHkr0HbdIevVAtWbZ-k_7dHM",
+    authDomain: "easyauction-18f3f.firebaseapp.com",
+    databaseURL: "https://easyauction-18f3f.firebaseio.com",
+    storageBucket: "easyauction-18f3f.appspot.com",
+  };
+firebase.initializeApp(config);
+ImageDealer.REF = firebase;
+
+var currentUser=null ;
+var currentUserid=null ;
+var picURL;
+//var viewModal = new ViewModal($("#view-modal"));
+var uploadModal = new UploadModal($("#upload-modal"));
+
+/* i add*/
+var fbProvider = new firebase.auth.FacebookAuthProvider();
+
+var currViewer = "";
+var nowItem = "";
+/* above is I add
+
+/*
+    分為三種使用情形：
+    1. 初次登入，改變成登入狀態
+    2. 已為登入狀態，reload 網站照樣顯示登入狀態
+    3. 未登入狀態
+
+    登入/當初狀態顯示可使用下方 logginOption function
+*/
+firebase.auth().onAuthStateChanged(function(user) {
+    console.log("state change enter"+user);
+    currentUser=user.displayName;
+      if(user!=null){
+        logginOption(true);
+        firebase.database().ref("Items/").once("value",reProduceAll) ;
+      }else{
+        logginOption(false);
+        
+      }
+ });
+
+function getItemByURL(suburl) {
+  return new Firebase("https://easyauction-18f3f.firebaseio.com/"+suburl);
+}
+
+$("#signin").click(function () {
+  // 登入後的頁面行為
+  
+  firebase.auth().signInWithPopup(fbProvider).then(function
+  (result) {
+   // console.log(firebase.database().ref("Users/"+result.user.displayName));
+    firebase.database().ref("Users/"+result.user.uid).update({name:result.user.displayName,ID:result.user.uid,photo:result.user.photoURL,sellItems: null, UserFirstLoginTime:new Date ($.now()).toLocaleString()});
+    currentUserid=result.user.uid;
+    currentUser=result.user.displayName;
+
+    
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessa = error.message;
+    console.log(errorCode,errorMessa);
+  })
+
+});
+
+$("#signout").click(function () {
+
+    // 登出後的頁面行為
+    firebase.auth().signOut().then(function() {
+      firebase.database().ref("Items/").once("value",reProduceAll) ;
+      console.log("click signout");
+      logginOption(false);
+      currentUser=null ;
+      currentUserid=null ;
+    },function(error) {
+      console.log(error.code);
+    });
+});
+
+
+$("#submitData").click(function () {
+    // 上傳新商品
+   // var newdata=firebase.database().ref("Items/").push({title:,price:,descrip:,seller:});
+ //   firebase.database().ref("Users/"+currentUser).update({items:newdata});
+  var dataArr = $("#item-info").serializeArray();
+  var picFile = $("#picData")[0].files[0];
+ // var picTrans = new FileReader();
+  if (dataArr[0].value != "" && dataArr[1].value != "" && dataArr[2].value != "" && picFile) {
+    //check if it is picture(not yet)
+    saveItems(dataArr[0].value, dataArr[1].value, dataArr[2].value);
+/*    picTrans.readAsDataURL(picFile);
+    picTrans.onloadend = (function (imge) {return function (e) {
+        imge.src = e.target.result;
+        saveItems(dataArr[0].value, dataArr[1].value, dataArr[2].value, e.target.result);
+    }})(picFile);*/
+ 
+    console.log("finish submit");
+    $("#upload-modal").modal('hide');
+  }
+
+});
+
+
+$("#editData").click(function (a) {
+    // 編輯商品資訊
+    var dataArr = $("#item-info").serializeArray();
+    var picFile = $("#picData")[0].files[0];
+    if (dataArr[0].value != "" && dataArr[1].value != "" && dataArr[2].value != "" ) {
+      editItems(dataArr[0].value, dataArr[1].value, dataArr[2].value,picFile);
+      $("#upload-modal").modal('hide');
+    }
+});
+
+$("#removeData").click(function () {
+    //刪除商品
+  removeItems();
+  $("#upload-modal").modal('hide');
+});
+
+/*
+    商品按鈕在dropdown-menu中
+    三種商品篩選方式：
+    1. 顯示所有商品
+    2. 顯示價格高於 NT$10000 的商品
+    3. 顯示價格低於 NT$9999 的商品
+
+*/
+
+
+function logginOption(isLoggin) {
+  if (isLoggin) {
+    $("#upload").css("display","block");
+    $("#signin").css("display","none");
+    $("#signout").css("display","block");
+  }else {
+    $("#upload").css("display","none");
+    $("#signin").css("display","block");
+    $("#signout").css("display","none");
+  }
+}
+
+firebase.database().ref("Items/").on("value",reProduceAll) ;
+function reProduceAll(allItems) {
+    /*
+    清空頁面上 (#item)內容上的東西。
+    讀取爬回來的每一個商品*/
+  var allData=allItems.val();
+  $("#items").empty();//清空原本#items內的東西
+  for(var itemData in allData)//抓取allData中的每一個資料夾(系統命名)
+  {
+    allData[itemData].itemKey= itemData;
+    produceSingleItem(allData[itemData]);
+  }
+}
+// 每點開一次就註冊一次
+function produceSingleItem(sinItemData){
+  /*
+    抓取 sinItemData 節點上的資料。
+    若你的sinItemData資料欄位中並沒有使用者名稱，請再到user節點存取使用者名稱
+    資料齊全後塞進item中，創建 Item 物件，並顯示到頁面上。
+  */
+  var item=sinItemData;
+//  firebase.database().ref("Items/"+sinItemData+"/").once("value",function (item) {
+    var currentUser = firebase.auth().currentUser;
+    var thisitem=item;
+  //  var thisitem=item.val();
+
+    var product = new Item({"title": thisitem.title, "price": thisitem.price, "itemKey": sinItemData.itemKey, "seller": thisitem.seller, 
+    "sellerName":thisitem.sellerName}, currentUser);
+    $("#items").append(product.dom);
+      /*
+        用 ViewModal 填入這筆 item 的資料
+        呼叫 ViewModal callImage打開圖片
+        創建一個 MessageBox 物件，將 Message 的結構顯示上 #message 裡。
+        */
+    var viewModal = new ViewModal($("#view-modal"));
+  //  console.log(sinItemData.itemKey+" "+ thisitem.seller)
+  //  viewModal.callImage(sinItemData.itemKey,thisitem.seller);
+ //   viewModal.callImage(item.key,thisitem.seller);
+ //   console.log(product.editBtn);
+    $(product.editBtn).click(function(snapshot){
+    //  updateModal(product.dom);
+      uploadModal.clear;
+    //  console.log("yes"+product.itemKey);
+      firebase.database().ref("Items/"+product.itemKey+"/").once("value",function (item) {
+             //   console.log( item.val().seller);
+        //        console.log( item.val().seller);
+                nowItem=product.itemKey;
+                uploadModal.callImage(product.itemKey,item.val().seller);
+                uploadModal.editData( item.val());
+              });
+    });
+    $(product.viewBtn).click(function(snapshot){
+    //  updateModal(product.dom);
+      viewModal.clear;
+      firebase.database().ref("Items/"+product.itemKey+"/").once("value",function (item) {
+     //   console.log(thisitem.seller);
+        viewModal.callImage(product.itemKey,thisitem.seller);
+        viewModal.writeData( item.val());
+        //viewModal.callImage(product.itemKey,item.val().seller);
+        //viewModal.writeData( item.val());
+      });
+   //   console.log(sinItemData.itemKey+"~");
+      var messBox = new MessageBox(firebase.auth().currentUser, sinItemData.itemKey); 
+      $("#message").append(messBox.dom);
+      generateDialog(sinItemData.itemKey, messBox);
+        /*
+          判斷使用者是否有登入，如果有登入就讓 #message 容器顯示輸入框。
+          在 MessageBox 上面註冊事件，當 submit 時將資料上傳。
+          */
+      
+      if (currentUser) 
+      {
+       // console.log(product.itemKey===sinItemData);
+        
+        $("#message").append(messBox.inputBox);
+       
+        messBox.inputBox.keypress(function (e) {
+          if (e.which == 13) 
+          {
+            e.preventDefault();
+      //      console.log($(this).find("#dialog").val());
+            var userdialog=$(this).find("#dialog").val();
+        //    console.log(product.itemKey);
+        //    console.log(messBox.userName);
+        //   console.log(firebase.auth().currentUser);
+            var currentUserid = firebase.auth().currentUser.uid;
+            var currentUserName=firebase.auth().currentUser.displayName;
+         //   var picURL;
+            firebase.database().ref("Users/"+currentUserid+"/").once("value",function (item) {
+                picURL=item.val().photo;
+          //      console.log(picURL);
+                var message={"message":userdialog, "userTime": new Date().getTime(), "talkerID": currentUserid,"talkerName":currentUserName,"picture":picURL};
+                var newmessage = firebase.database().ref("Items/").child(product.itemKey+"/Messages");
+                //firebase.database.ref("Items").off();
+                newmessage.push(message);
+                viewModal.callImage(sinItemData.itemKey,thisitem.seller);
+                //messBox.addDialog({"message":userdialog, "time": new Date().getTime(), "name": currentUserName,"picURL":picURL});
+               // $("#message").append(messBox.dom);
+            });            
+                
+            $(this).find("#dialog").val("");
+
+            }
+        });
+      }//end if currenUser
+      
+    });
+    /*
+    從資料庫中抓出message資料，並將資料填入MessageBox
+    */
+    
+
+ // });
+
+    /*
+    如果使用者有登入，替 editBtn 監聽事件，當使用者點選編輯按鈕時，將資料顯示上 uploadModal。
+    */
+
+
+}
+//new Date().getTime()
+function generateDialog(diaDatakey, messageBox) {
+    
+    firebase.database().ref("Items/"+diaDatakey+"/Messages/").orderByChild("userTime").on("value",function(data) {
+ //     console.log(data.val());
+      messageBox.refresh();
+      var messages = data.val()
+      for(var messageKey in messages){
+        if(messages.hasOwnProperty(messageKey)){
+          var singleMessage = messages[messageKey];
+          messageBox.addDialog({"message":singleMessage.message, "time": singleMessage.userTime, "name": singleMessage.talkerName,"picURL":singleMessage.picture});
+         // $("#message").append(messBox.inputBox);
+        }
+      }
+    });
+ //   var messBox = new MessageBox(item.val().talkerID, diaDatakey); 
+  //  messBox.addDialog({"message":userdialog, "time": new Date().getTime(), "name": currentUserName,"picURL":picURL});
+}
+
+/*-------------------------------------------------------------------------*/
+function saveItems(title, price, descrip) {
+  console.log("upload");
+  var currentUserid = firebase.auth().currentUser.uid;
+  var currentUserName=firebase.auth().currentUser.displayName;
+  var data={"title":title, "price":parseInt(price), "descrip":descrip, "userTime": new Date($.now()).toLocaleString(), "seller": currentUserid,"sellerName":currentUserName};
+  var itemID = firebase.database().ref("Items/").push(data).key;
+  var updateitem = firebase.database().ref("Users/").child(currentUserid+"/sellItems/"+itemID);
+  updateitem.set(data);
+  uploadModal._currentItemKey=itemID;
+  uploadModal.submitPic(firebase.auth().currentUser.uid);
+}
+
+
+function editItems(title, price, descrip,isNewPic) {
+  console.log("edit");
+    firebase.database().ref("Items/").child(nowItem).once("value",function (item) {
+   // uploadModal.callImage(nowItem, item.val().seller);
+     uploadModal.upload;
+     if(isNewPic!=null)
+     {
+        uploadModal._currentItemKey=nowItem;
+        uploadModal.submitPic(item.val().seller);
+     }
+
+    firebase.database().ref("Items/").child(nowItem).update({"title":title, "price":parseInt(price), "descrip":descrip, "userTime":new Date ($.now()).toLocaleString()});
+    firebase.database().ref("Users/"+item.val().seller+"/sellItems/").child(nowItem).update({"title":title, "price":parseInt(price), "descrip":descrip, "userTime":new Date ($.now()).toLocaleString()});
+    });
+
+  }
+
+
+
+function removeItems() {
+  console.log(nowItem);
+  firebase.database().ref("Items").child(nowItem).once("value",function (item) {
+    var userid=item.val().seller;
+   // console.log(userid);
+    console.log(item.val().seller+"!!!!!!");
+    firebase.database().ref("Users/"+userid+"/sellItems/"+nowItem).remove();
+    uploadModal._currentItemKey=nowItem;
+    uploadModal.deletePic(userid);
+  });
+  var curr=firebase.database().ref("Items").child(nowItem);
+  curr.remove();
+}
+
+$("#SelectPrice li:nth-of-type(1)").click(function (event) {
+  firebase.database().ref("Items").once("value",reProduceAll);
+});
+
+$("#SelectPrice li:nth-child(2)").click(function (event) {
+  firebase.database().ref("Items").orderByChild("price").startAt(10000).once("value",reProduceAll);//排序資料，並抓取10000以上
+});
+
+$("#SelectPrice li:nth-of-type(3)").click(function (event) {
+  firebase.database().ref("Items").orderByChild("price").endAt(9999).once("value",reProduceAll);//排序資料，並抓取9999以下
+});
+
+function viewAllItems() {
+  firebase.database().ref("Items").once("value",reProduceAll);
+}
+
+
+$("#upload-modal").on('hidden.bs.modal', function (e) {
+  $("#item-info :input").val("");
+  $("#picData").val("");
+  $("#ModalLabel").text("New Item");
+  $("#editData").css("display","none");
+  $("#removeData").css("display","none");
+  $("#submitData").css("display","inline-block");
+});
